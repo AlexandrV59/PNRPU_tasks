4e43ccda7f17f159678f01acbfcf6e0bf78b0337
AlexndrV59 <kent1234810@gmail.com>
2020-11-12 21:28:06 +0500

    All right (no)

---
+++ 2_7_3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ;
+    cin >> side;
+
+    for (int i = 1; i <= side; i++) 
+    {
+        for (int j = 1; j <= side; j++) 
+        {
+            if (i == 1 || i == side) 
+                cout << '*';
+            else 
+            {
+                if (j == 1 || j == side) 
+                    cout << '*';
+                else 
+                    cout << ' ';
+            }
+        }  
+        cout << endl; 
+    }  
+
+    return 0;
+}
