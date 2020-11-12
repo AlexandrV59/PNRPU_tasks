ef2ea41a558d930da47aac5c29c9e46a538abfb7
AlexndrV59 <kent1234810@gmail.com>
2020-11-12 21:04:54 +0500

    The last one...

---
+++ 2_7_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int n;
+int main()
+{
+    cin >> n;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            cout << "(^-^)"; //like a smile 
+            cout << " ";
+        }
+        cout << "\n"<<"\n";
+    }
+    return 0;
+}
